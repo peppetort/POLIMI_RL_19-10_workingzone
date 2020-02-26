11ca62552c1c80431a5ab4959abe2b5d1289d3dd
root <luca.rigutti@outlook.it>
2020-02-26 18:06:14 +0100

    Sistemazione file e commentato

--- source_code_project.vhd
+++ source_code_project.vhd
@@ -48,7 +48,7 @@ entity source_code_project is
 end source_code_project;
 
 architecture Behavioral of source_code_project is
-signal wz0 : std_logic_vector(7 downto 0);
+signal wz0 : std_logic_vector(7 downto 0); --Queste sono le "variabili" dove mettiamo le Working Zone
 signal wz1 : std_logic_vector(7 downto 0);
 signal wz2 : std_logic_vector(7 downto 0);
 signal wz3 : std_logic_vector(7 downto 0);
@@ -56,7 +56,7 @@ signal wz4 : std_logic_vector(7 downto 0);
 signal wz5 : std_logic_vector(7 downto 0);
 signal wz6 : std_logic_vector(7 downto 0);
 signal wz7 : std_logic_vector(7 downto 0);
-signal contatoreWZReading  : integer;
+signal contatoreWZReading  : integer; --Questo è un contatore per sapere a quale working zone sto leggendo
 signal status : integer; --status: 0->Leggo input, 1->Scrivo l'output, 2->Disabilito o_done, questo perchè o_done deve rimanese in alto fino a quando i_start non sia sceso
 
 begin
@@ -65,12 +65,17 @@ begin
 
     if (rising_edge(i_clk)) then
         if(i_rst = '1') then
-            contatoreWZReading<=0;
-            o_address <= std_logic_vector(to_unsigned(0,o_address'length)); --Non so bene come specificare che o_adress deva indicare la cella numero [0]
+            --Qui ho appena ricevuto il reset, quindi devo caricare tutte le working zone in memoria
+            --Allora chiedo la prima working zone e abilito la memoria ad effettura le operazioni solo in lettura
+            contatoreWZReading<=1;
+            o_address <= std_logic_vector(to_unsigned(0,o_address'length));
             o_en <= '1';
+            o_we <= '0';
             status <=0;
         end if;
         case contatoreWZReading is
+        --Questo case mi serve per leggere una a una le varie working zone, usando il contatore per sapere dove sono
+        --Questa è la soluzione più semplice che mi sia venuta in mente, anche se quella con più codice
                 when 1 =>
                     wz0 <= i_data;
                     o_address <= std_logic_vector(to_unsigned(1,o_address'length));
@@ -100,117 +105,156 @@ begin
                     o_address <= std_logic_vector(to_unsigned(7,o_address'length));
                     contatoreWZReading  <= 8;
                 when 8 =>
+                    --Questa è l'ultima working zone, quindi disabilito la memoria a fare operazioni
                     wz7<=i_data;
                     o_en<='0';
+                    
          end case;
 
 
 
 
+        --Questa if mi serve per sapere se ho letto tutte le working zone, quindi a questo punto se i_start è attivo inizio a processare
+        if contatoreWZReading = 8 then
+          
+            case status is
+              when 0 =>
+                  --Qui faccio richiesta della cella dove c'è il dato che si leggerà nel prossimo ciclo
+                  o_address <= std_logic_vector(to_unsigned(8,o_address'length)); --Vado a leggere nella 8° zona il valore di input
+                  o_en <= '1';
+                  if i_start = '1' then
+                    status<=1;
+                  end if;
+              when 1 =>
+              --Qui dovrebbe esserci in i_data il nostro valore da processare, inizia una serie di if contatenati
+              --Il procedimento è questo, controllo se il i_data - wzX sia compreso tra 0 e 3
+              --Allora se lo è metto in o_data il valore della wzX con X codificato, quindi "1JJJ0000", con JJJ (2) = X (10)
+              --Poi con un case controllo la differenza, "traducendo" il valore che ottengo in onehot
+              --Else fino all'ultima wz e nel caso non fosse in nessuna passo direttamente il valore d'ingresso in uscita
+                if i_data - wz0 >0 and i_data - wz0 <3 then 
+                    o_data <= "10000000";
+                    case i_data - wz0 is
+                        when "00000000" =>
+                            o_data <= i_data + "00000001";
+                        when "00000001" =>
+                            o_data <= i_data + "00000010";
+                        when "00000010" =>
+                            o_data <= i_data + "00000100";
+                        when "00000011" =>
+                            o_data <= i_data + "00001000";
+                    end case;
+                    
+                else
+                    if i_data - wz1 >0 and i_data - wz1 <3 then
+                    
+                        o_data <= "10010000";
+                        case i_data - wz1 is
+                            when "00000000" =>
+                                o_data <= i_data + "00000001";
+                            when "00000001" =>
+                                o_data <= i_data + "00000010";
+                            when "00000010" =>
+                                o_data <= i_data + "00000100";
+                            when "00000011" =>
+                                o_data <= i_data + "00001000";
+                        end case;
+                    else
+                        if i_data - wz2 >0 and i_data - wz2 <3 then
+                    
+                        o_data <= "10100000";
+                        case i_data - wz2 is
+                            when "00000000" =>
+                                o_data <= i_data + "00000001";
+                            when "00000001" =>
+                                o_data <= i_data + "00000010";
+                            when "00000010" =>
+                                o_data <= i_data + "00000100";
+                            when "00000011" =>
+                                o_data <= i_data + "00001000";
+                        end case;
+                        else
+                            if i_data - wz3 >0 and i_data - wz3 <3 then
+                                
+                                o_data <= "10110000";
+                                case i_data - wz3 is
+                                    when "00000000" =>
+                                        o_data <= i_data + "00000001";
+                                    when "00000001" =>
+                                        o_data <= i_data + "00000010";
+                                    when "00000010" =>
+                                        o_data <= i_data + "00000100";
+                                    when "00000011" =>
+                                        o_data <= i_data + "00001000";
+                                end case;
+                            else
+                                if i_data - wz4 >0 and i_data - wz4 <3 then
+                                    o_data <= "11000000";
+                                    case i_data - wz4 is
+                                        when "00000000" =>
+                                            o_data <= i_data + "00000001";
+                                        when "00000001" =>
+                                            o_data <= i_data + "00000010";
+                                        when "00000010" =>
+                                            o_data <= i_data + "00000100";
+                                        when "00000011" =>
+                                        o_data <= i_data + "00001000";
+                                    end case;
+                                else
+                                    if i_data - wz5 >0 and i_data - wz5 <3 then
+                                        o_data <= "11010000";
+                                        case i_data - wz5 is
+                                            when "00000000" =>
+                                                o_data <= i_data + "00000001";
+                                            when "00000001" =>
+                                                o_data <= i_data + "00000010";
+                                            when "00000010" =>
+                                                o_data <= i_data + "00000100";
+                                            when "00000011" =>
+                                        o_data <= i_data + "00001000";
+                                        end case;
+                                    else
+                                        if i_data - wz6 >0 and i_data - wz6 <3 then
+                                            o_data <= "11100000";
+                                            case i_data - wz6 is
+                                                when "00000000" =>
+                                                    o_data <= i_data + "00000001";
+                                                when "00000001" =>
+                                                    o_data <= i_data + "00000010";
+                                                when "00000010" =>
+                                                    o_data <= i_data + "00000100";
+                                                when "00000011" =>
+                                                    o_data <= i_data + "00001000";
+                                             end case;
+                                        else
+                                            if i_data - wz7 >0 and i_data - wz7 <3 then
+                                                o_data <= "11110000";
+                                                case i_data - wz7 is
+                                                    when "00000000" =>
+                                                        o_data <= i_data + "00000001";
+                                                    when "00000001" =>
+                                                        o_data <= i_data + "00000010";
+                                                    when "00000010" =>
+                                                        o_data <= i_data + "00000100";
+                                                    when "00000011" =>
+                                                        o_data <= i_data + "00001000";
+                                                 end case;
+                                            else
+                                                o_data <= i_data;
+                                            end if;
+                o_we <= '1';
+                o_done <= '1';
+                status<=2;
+                o_address <= "00001000"; -- Metto o_address a 8
+              when 2 =>
+                  if i_start = '0' then
+                    o_done <= '0';
+                    status <=  0;
+                  end if;
+              end case;
 
-        if(contatoreWZReading==8)then
-        case status is
-          when 0 =>
-              o_adress <= 8; --Vado a leggere nella 8° zona il valore di input
-              o_en <= 1;
-              if(i_start=1) then
-                status<=1;
-              end if;
-          when 1 =>
-            --Qui va l'elaborazione del dato, a questo punto creo un entita che passandogli le 8 wz e l'indirizzo di base elabora per darmi il risultato
+        end if;
 
-
-
-            o_we <= 1;
-            o_done <=1;
-            status<=2;
-          when 2 =>
-          if(i_start=0) then
-            o_done=0;
-            status<=0;
-          end if;
-          end case;
-
-        endif;
-
-
-
-
-
-
-
-
-
-
--- Instanzo un contatore da 0 a 7, con un bit di fine
-
-
--- Quando mi arriva il bit di reset, faccio ripartire il contatore, da li faccio 7 cicli di clock per riempire i signal
-
--- Aspetto che i_start sia ad 1,
--- A quel punto si parte nella lettura, quindi o_address deve andare all'ottava posizione della RAM, o_en = 1 e o_we = 0
--- Ricevo su i_data l'indirizzo che devo condificare, quindi devo prenderlo e fare le operazioni
 
     end if;
-end Behavioral;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-entity encoder_logic is
-  Port (
-    wz0               : in std_logic_vector(7 downto 0);
-    wz1               : in std_logic_vector(7 downto 0);
-    wz2               : in std_logic_vector(7 downto 0);
-    wz3               : in std_logic_vector(7 downto 0);
-    wz4               : in std_logic_vector(7 downto 0);
-    wz5               : in std_logic_vector(7 downto 0);
-    wz6               : in std_logic_vector(7 downto 0);
-    wz7               : in std_logic_vector(7 downto 0);
-    input_address     : in std_logic_vector(7 downto 0);
-    output_address    : out std_logic_vector(7 downto 0);
- );
-end encoder_logic;
-
-architecture Behavioral of encoder_logic is
-
-begin
-
---Non so come implementarlo, la mia idea è di creare un componente che mi da l'output solo se questo è
---Questo perchè vorrei scrivere del codice pulito, molto semplice da controllare
-
---Se potessi creare un componente che passandogli in input (wz,"Numero wz", input_address) come output mi dia ((booleano)is_in, output_address )
---Così in questa entity faccio if(is_in) then output_address<=output_address else if così via fino a che l'ultimo else faccio output_address<=input_address
-
+    end process;
 end Behavioral;
